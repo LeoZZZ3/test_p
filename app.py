4ea82227364a15bc0338c7da6015f68dde91a0f9
LeoZZZ3 <zagoleo2002@gmail.com>
2026-04-09 11:16:37 +0200

    add random number endpoint

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return "Salut", 200, {"Content-Type": "text/plain"}
 
+@app.route("/random-number", methods=["GET"])
+def random_number():
+    import random
+    number = random.randint(1, 100)
+    return str(number), 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
